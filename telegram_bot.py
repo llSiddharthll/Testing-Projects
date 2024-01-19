40d831ab6a861335f5ca7773fd8529e758440d26
llSiddharthll <siddharthgreat443@gmail.com>
2024-01-20 00:26:46 +0530

    n

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,35 +1,48 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
+from telegram.ext import (
+    ApplicationBuilder,
+    ContextTypes,
+    CommandHandler,
+    filters,
+    MessageHandler,
+)
 import requests
 import os
-API_URL = "https://api-inference.huggingface.co/models/TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+API_URL = (
+    "https://api-inference.huggingface.co/models/TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+)
 headers = {"Authorization": "Bearer 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"}
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
+
 def query(payload):
-    formatted_payload = [
-            {"role": "system", "content": "You are a friendly chatbot who always responds in the style of a pirate"},
-            {"role": "user", "content": payload}
-        ]
+    formatted_payload =f"<|system|>
+        You are a chatbot who can help code!</s>
+        <|user|>
+        {payload}</s>
+        <|assistant|>"
     response = requests.post(API_URL, headers=headers, json=formatted_payload)
     return response.json()
-	
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_input = update.message.text
-    output = query({"input" : user_input})
+    output = query({"input": user_input})
     generated_text = output
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=generated_text)
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=generated_text
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         # Initialize your application
         application = ApplicationBuilder().token(TOKEN).build()
 
         # Set up the handlers
-        start_handler = CommandHandler('bro', start)
+        start_handler = CommandHandler("bro", start)
         chat_handler = MessageHandler(filters.TEXT, start)
 
         # Add the handlers to the application
