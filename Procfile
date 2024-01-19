c018c4cd90059b8670ffb54e2f89cae282798ebf
llSiddharthll <siddharthgreat443@gmail.com>
2024-01-19 19:36:13 +0530

    done

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-worker: python telegram_bot.py
 worker: python discord_bot.py
--- discord_bot.py
+++ discord_bot.py
@@ -1,9 +1,11 @@
 import discord
 import requests
+import os
 
 API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
 headers = {"Authorization": "Bearer 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"}
 
+TOKEN = os.environ.get('DISCORD_TOKEN')
 
 def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
@@ -30,4 +32,4 @@ class MyClient(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
-client.run('MTEzMzg5OTIxODY0NjQ4NzIwMQ.GDO0L6.l_24WDCuycEO98Fe6hbyh0RxwV6PrCuOZr1F8Y')
+client.run(TOKEN)
