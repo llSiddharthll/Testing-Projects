0d27cff19f345249bdf2728b784e8091e1a5701a
llSiddharthll <siddharthgreat443@gmail.com>
2024-01-20 00:31:07 +0530

    n

--- telegram_bot.py
+++ telegram_bot.py
@@ -18,12 +18,12 @@ TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
 
 def query(payload):
-    formatted_payload =(f"<|system|>
+    formatted_payload =f"""<|system|>
         You are a chatbot who can help code!</s>
         <|user|>
         {payload}</s>
-        <|assistant|>")
-    response = requests.post(API_URL, headers=headers, json=formatted_payload)
+        <|assistant|>"""
+    response = requests.post(API_URL, headers=headers, json={"inputs":formatted_payload})
     return response.json()
 
 
