beaaf3630f5e73c2fbe9bf85fcd8e3a5738eb16f
llSiddharthll <siddharthgreat443@gmail.com>
2024-03-01 11:35:37 +0530

    updated

--- tg_bot.py
+++ tg_bot.py
@@ -11,50 +11,75 @@ from telegram.ext import (
 import io
 import requests
 
-API_URL = "https://api-inference.huggingface.co/models/openchat/openchat-3.5-0106"
-IMAGE_API_URL = "https://api-inference.huggingface.co/models/segmind/Segmind-Vega"
-headers = {"Authorization": "Bearer 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"}
-TOKEN = os.environ.get('TELEGRAM_TOKEN')
 
-def query(payload):
-    formatted_payload = f"""
-        GPT4 Correct User: Hello<|end_of_turn|>
-        GPT4 Correct Assistant: Hi<|end_of_turn|>
-        GPT4 Correct User: What is your name?<|end_of_turn|>
-        GPT4 Correct Assistant: My name is "Itachi Uchiha" of the village "leaf", I am a conversational bot made by Siddharth<|end_of_turn|>
-        GPT4 Correct User: {payload}<|end_of_turn|>
-        GPT4 Correct Assistant: 
-        """
-    response = requests.post(
-        API_URL, headers=headers, json={"inputs": formatted_payload}
-    )
-    return response.json()
-	
+def GeminiChat(prompt):
+    # Replace "YOUR_API_KEY" with your actual API key
+    API_KEY = os.environ.get("Gemini_Key")
+
+    # Define the request data
+    data = {
+        "contents": [
+            {
+                "role": "user",
+                "parts": [{"text": prompt}],
+            }
+        ]
+    }
+
+    # Set the request headers
+    headers = {"Content-Type": "application/json"}
+
+    # Construct the API endpoint URL with the API key
+    url = f"https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key={API_KEY}"
+
+    # Send the POST request
+    response = requests.post(url, headers=headers, json=data)
+
+    # Check for successful response
+    if response.status_code == 200:
+        # Parse the JSON response
+        response_data = response.json()
+        generated_text = response_data["candidates"][0]["content"]["parts"][0]["text"]
+        # Access the generated text from the response
+        return generated_text
+
+    else:
+        return {"Error": response.status_code}
+
+
+IMAGE_API_URL = (
+    "https://api-inference.huggingface.co/models/cagliostrolab/animagine-xl-3.0"
+)
+headers = {"Authorization": "Bearer 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"}
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
+
+
 def query_image(payload):
     response = requests.post(IMAGE_API_URL, headers=headers, json=payload)
     return response.content
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_input = update.message.text.lower()
 
     if user_input.startswith(("jade", "/bro", "bro", "bot")):
-        await context.bot.send_chat_action(chat_id=update.effective_chat.id, action="typing")
+        await context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action="typing"
+        )
 
-        user_input = ' '.join(user_input.split()[1:])
-        output = query(user_input)
-        generated_text = output[0]["generated_text"]
-        output_index = generated_text.find(user_input)
-        output_text = generated_text[output_index + len(user_input):]
-        lines = output_text.split('\n')
-        result_output = '\n'.join(lines[2:])
+        user_input = " ".join(user_input.split()[1:])
+        output = GeminiChat(user_input)
+
         await context.bot.send_message(
             chat_id=update.effective_chat.id,
-            text=result_output,
+            text=output,
             reply_to_message_id=update.message.message_id,
         )
 
     elif user_input.startswith(("generate", "make")):
-        await context.bot.send_chat_action(chat_id=update.effective_chat.id, action="typing")
+        await context.bot.send_chat_action(
+            chat_id=update.effective_chat.id, action="typing"
+        )
         try:
             image_bytes = query_image({"inputs": user_input})
             image = io.BytesIO(image_bytes)
@@ -69,7 +94,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
             logging.error(f"Error generating image: {e}")
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
-                text = "Sorry, I am loading the generative AI model right now, please wait a min.",
+                text="Sorry, I am loading the generative AI model right now, please wait a min.",
                 reply_to_message_id=update.message.message_id,
             )
 
